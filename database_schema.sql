72e7c35a8eaf1f17b225243f8f12672823f7fea1
Nichapha Noichan <fayeren.iouuu@gmail.com>
2026-06-04 16:07:39 +0700

    Edit

--- database_schema.sql
+++ database_schema.sql
@@ -1,13 +1,13 @@
 create table Users (
     id integer primary key autoincrement,
     username text not null,
-    email text
+    email text not null
 );
 
-create table  Reservations (
+create table Reservations (
     booking_id integer primary key autoincrement,
     user_id integer not null,
     booking_time text not null,
-    status text default "Comfirmed",
+    status text default 'Confirmed',
     foreign key (user_id) references Users(id)
 );
